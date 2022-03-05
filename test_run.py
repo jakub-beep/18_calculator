f0bb311f8dc86cf1bbb53132743d4d282660a6d6
jakub wytykowski <jakub.wytykowski@gmail.com>
2022-03-05 18:53:55 +0100

    adding tests

--- test_run.py
+++ test_run.py
@@ -1,15 +1,25 @@
-if __name__ == "__main__":
-    import logic
-    import charts
-    import pytest
+import logic
+import pytest
 
-    def test_estimate_function():
-        result = logic_2.calc_sin.estimate_function('sin')
-        assert result == 0.5
 
-# def adding(x, y):
-#     return x+ y
+@pytest.mark.parametrize("test_input,expected", [('+', '+'), ('-', '-'), ('*', '*'), ('/', '/')])
+def test_add_to_calculation(test_input, expected):
+    result = logic.calc.add_to_calculation(test_input)
+    assert result == expected
+
+
+def test_evaluate_calculation():
+    result = logic.calc.evaluate_calculation()
+    assert result == '45'
+
+
+def test_clear_field():
+    result = logic.calc.clear_field()
+    assert result == ''
+
+
+def test_estimate_function_sin():
+    result_sin = logic.calc_sin.estimate_function()
+    result_cos = logic.calc_cos.estimate_function()
+    assert result_sin == '1.0' and result_cos == '0.0'
 #
-# def test_adding():
-#     result = adding(2, 5)
-#     assert result == 6
