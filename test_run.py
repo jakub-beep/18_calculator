aec1ad2f08c254d1fd63143c0bc5f7bc18bd6cfb
jakub wytykowski <jakub.wytykowski@gmail.com>
2022-03-24 18:00:19 +0100

    adding 2 actually running tests

--- test_run.py
+++ test_run.py
@@ -1,25 +1,20 @@
 import logic
 import pytest
+from unittest.mock import Mock
+import math
+import tkinter as tk
 
 
 @pytest.mark.parametrize("test_input,expected", [('+', '+'), ('-', '-'), ('*', '*'), ('/', '/')])
 def test_add_to_calculation(test_input, expected):
-    result = logic.calc.add_to_calculation(test_input)
-    assert result == expected
+    calc_test = logic.Calculation(Mock())
+    calc_test.add_to_calculation(test_input)
+    assert calc_test.calculation == expected
 
 
-def test_evaluate_calculation():
-    result = logic.calc.evaluate_calculation()
-    assert result == '45'
-
-
-def test_clear_field():
-    result = logic.calc.clear_field()
-    assert result == ''
-
-
-def test_estimate_function_sin():
-    result_sin = logic.calc_sin.estimate_function()
-    result_cos = logic.calc_cos.estimate_function()
-    assert result_sin == '1.0' and result_cos == '0.0'
-#
+@pytest.mark.parametrize("test_input,expected", [('2+2', '4')])
+def test_evaluate_calculation(test_input, expected):
+    calc_test = logic.Calculation('Mock()')
+    calc_test.calculation = test_input
+    calc_test.evaluate_calculation()
+    assert calc_test.calculation == expected
